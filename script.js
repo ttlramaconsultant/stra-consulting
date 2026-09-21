27be3c34aaae34ae754f2878d787059000b288d8
STRA Consulting Services - Telecom Revenue Assurance Consulting <ttl.rama@gmail.com>
2026-09-21 17:27:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const menuButton=document.querySelector('.menu-toggle');const nav=document.querySelector('.site-header nav');if(menuButton){menuButton.addEventListener('click',()=>nav.classList.toggle('open'));}document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
